bdb9bf20c25bcc4ad34d13d77b709126ac41f5d3
Filiph Siitam Sandström <filiph@nordcom.io>
2023-09-15 12:45:10 +0200

    Tests: Add stub tests.

--- src/android-sdk/install.sh
+++ src/android-sdk/install.sh
@@ -1,17 +1,50 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+set +H
 
 URL_SDK="https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip"
 
+# Options.
+PLATFORM=$platform
+if [ -z "$PLATFORMS" ]; then
+    PLATFORM="34"
+fi
+
 DEBIAN_FRONTEND="noninteractive" sudo apt update &&
-    sudo apt install --no-install-recommends -y openjdk-22-jdk-headless unzip wget &&
+    sudo apt install --no-install-recommends -y openjdk-17-jdk-headless unzip wget &&
     apt clean
 
-mkdir -p "$ANDROID_HOME/$FOLDER"
+mkdir -p "$ANDROID_HOME"
 chown -R "$_REMOTE_USER:$_REMOTE_USER" "$ANDROID_HOME"
 
 su - "$_REMOTE_USER"
 
+cd $ANDROID_HOME
+
 wget -q "$URL_SDK" -O sdk.zip
-unzip -q sdk.zip -d "$ANDROID_HOME/$FOLDER/latest"
-rm -rf sdk.zip
+unzip sdk.zip
+rm sdk.zip
+
+mkdir -p $ANDROID_HOME/cmdline-tools/latest
+cd $ANDROID_HOME/cmdline-tools
+shopt -s extglob dotglob
+mv !(latest) latest
+shopt -u dotglob
+
+cd $ANDROID_HOME
+
+export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
+
+# Save original JAVA_HOME.
+OG_JAVA_HOME=$JAVA_HOME
+
+# thanks https://askubuntu.com/questions/772235/how-to-find-path-to-java#comment2258200_1029326.
+export JAVA_HOME=$(dirname $(dirname $(update-alternatives --list javac 2>&1 | head -n 1)))
+
+# Update everything to future-proof for the link getting stale.
+yes | sdkmanager "cmdline-tools;latest"
+# Download the platform tools.
+yes | sdkmanager "platform-tools" "platforms;android-$PLATFORM"
+
+# Restore JAVA_HOME.
+export JAVA_HOME=$OG_JAVA_HOME
